a146191e67e684699404267df26cdb515ac5d998
Vinay Roy <vroy@codeaurora.org>
2012-11-29 10:56:11 +0530

    msm: kgsl: Add max and min power level controls in sysfs
    Add min_pwrlevel, max_pwrlevel and thermal_pwrlevel to give a
    privileged user more control over which power levels are considered
    during DCVS power management.  max_pwrlevel is the maximum power
    level that the system can go to at any time, min_pwrlevel represents
    the lowest power level.  DCVS will chose any level between these
    two extremes.  thermal_pwrlevel allows a daemon to set an absolute
    top for frequency to prevent thermal issues. The effective maximum
    power level is considered to be the lower of thermal or max.
    Also added is num_pwrlevels that shows the number of active power
    levels. This corresponds to the gpu frequencies in
    available_gpu_frequencies.
    
    Change-Id: Ifc55256a6fec96ba1fbf8fd6c0fa1948173283b8
    Signed-off-by: Vinay Roy <vroy@codeaurora.org>

--- drivers/gpu/msm/kgsl_pwrctrl.h
+++ drivers/gpu/msm/kgsl_pwrctrl.h
@@ -27,15 +27,40 @@
 
 struct platform_device;
 
-struct kgsl_busy {
-	struct timeval start;
-	struct timeval stop;
-	int on_time;
-	int time;
-	int on_time_old;
-	int time_old;
+struct kgsl_clk_stats {
+	unsigned int old_clock_time[KGSL_MAX_PWRLEVELS];
+	unsigned int clock_time[KGSL_MAX_PWRLEVELS];
+	unsigned int on_time_old;
+	ktime_t start;
+	ktime_t stop;
 	unsigned int no_nap_cnt;
+	unsigned int elapsed;
+	unsigned int elapsed_old;
 };
+
+/**
+ * struct kgsl_pwrctrl - Power control settings for a KGSL device
+ * @interrupt_num - The interrupt number for the device
+ * @ebi1_clk - Pointer to the EBI clock structure
+ * @grp_clks - Array of clocks structures that we control
+ * @power_flags - Control flags for power
+ * @pwrlevels - List of supported power levels
+ * @active_pwrlevel - The currently active power level
+ * @thermal_pwrlevel - maximum powerlevel constraint from thermal
+ * @max_pwrlevel - maximum allowable powerlevel per the user
+ * @min_pwrlevel - minimum allowable powerlevel per the user
+ * @num_pwrlevels - number of available power levels
+ * @interval_timeout - timeout in jiffies to be idle before a power event
+ * @strtstp_sleepwake - true if the device supports low latency GPU start/stop
+ * @gpu_reg - pointer to the regulator structure for gpu_reg
+ * @gpu_cx - pointer to the regulator structure for gpu_cx
+ * @pcl - bus scale identifier
+ * @nap_allowed - true if the device supports naps
+ * @idle_needed - true if the device needs a idle before clock change
+ * @irq_name - resource name for the IRQ
+ * @restore_slumber - Flag to indicate that we are in a suspend/restore sequence
+ * @clk_stats - structure of clock statistics
+ */
 
 struct kgsl_pwrctrl {
 	int interrupt_num;
@@ -46,6 +71,8 @@ struct kgsl_pwrctrl {
 	unsigned int active_pwrlevel;
 	int thermal_pwrlevel;
 	unsigned int default_pwrlevel;
+	unsigned int max_pwrlevel;
+	unsigned int min_pwrlevel;
 	unsigned int num_pwrlevels;
 	unsigned int interval_timeout;
 	bool strtstp_sleepwake;
@@ -56,8 +83,8 @@ struct kgsl_pwrctrl {
 	unsigned int idle_needed;
 	const char *irq_name;
 	s64 time;
-	struct kgsl_busy busy;
 	unsigned int restore_slumber;
+	struct kgsl_clk_stats clk_stats;
 };
 
 void kgsl_pwrctrl_irq(struct kgsl_device *device, int state);
