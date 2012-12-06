783f44f5bd7a1b84036d798c1c3eb089b1212c0b
Tirupathi Reddy <tirupath@codeaurora.org>
2012-12-06 08:38:09 +0530

    msm: cpr: Add success message in probe
    Kernel log doesn't have success message of CPR initialization.
    Add a success message in the probe. By seeing kernel logs one
    can identify CPR is running or not.
    
    Change-Id: Ie326266ee0cc7c4ce99bc588297b2efb6bdee35b
    Signed-off-by: Tirupathi Reddy <tirupath@codeaurora.org>

--- arch/arm/mach-msm/msm_cpr.c
+++ arch/arm/mach-msm/msm_cpr.c
@@ -1067,6 +1067,8 @@ static int __devinit msm_cpr_probe(struct platform_device *pdev)
 	cpufreq_register_notifier(&cpr->freq_transition,
 					CPUFREQ_TRANSITION_NOTIFIER);
 
+	pr_info("CPR: driver registered successfully\n");
+
 	return res;
 
 err_cpr_config:
