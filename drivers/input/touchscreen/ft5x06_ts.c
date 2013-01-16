e171098c9c7aa8ae5371deb32f3230e03301fc20
Shantanu Jain <shjain@codeaurora.org>
2013-01-16 15:52:09 +0530

    input: ft5x06_ts: Add retry loops for controller register reads
    Retry i2c communication multiple times to make sure the
    device is connected and avoid corner cases of i2c failures.
    
    Change-Id: Iad504652f559ed7527d69e7b6bcb753400eecf38
    Signed-off-by: Shantanu Jain <shjain@codeaurora.org>

--- drivers/input/touchscreen/ft5x06_ts.c
+++ drivers/input/touchscreen/ft5x06_ts.c
@@ -35,8 +35,10 @@
 
 #define CFG_MAX_TOUCH_POINTS	5
 
-#define FT_STARTUP_DLY		150
+#define FT_STARTUP_DLY		250
 #define FT_RESET_DLY		20
+#define FT_DELAY_DFLT		20
+#define FT_NUM_RETRY		10
 
 #define FT_PRESS		0x7F
 #define FT_MAX_ID		0x0F
@@ -415,6 +417,7 @@ static int ft5x06_ts_probe(struct i2c_client *client,
 	u8 reg_value;
 	u8 reg_addr;
 	int err;
+	int tries;
 
 	if (!pdata) {
 		dev_err(&client->dev, "Invalid pdata\n");
@@ -531,25 +534,49 @@ static int ft5x06_ts_probe(struct i2c_client *client,
 	msleep(FT_STARTUP_DLY);
 
 	/*get some register information */
+	/* read firmware version */
 	reg_addr = FT5X06_REG_FW_VER;
-	err = ft5x06_i2c_read(client, &reg_addr, 1, &reg_value, 1);
-	if (err)
-		dev_err(&client->dev, "version read failed");
+	tries = FT_NUM_RETRY;
+	do {
+		err = ft5x06_i2c_read(client, &reg_addr, 1, &reg_value, 1);
+		msleep(FT_DELAY_DFLT);
+	} while ((err < 0) && tries--);
 
+	if (err < 0) {
+		dev_err(&client->dev, "version read failed");
+		goto free_reset_gpio;
+	}
 	dev_info(&client->dev, "[FTS] Firmware version = 0x%x\n", reg_value);
 
+	/* Read report rate */
 	reg_addr = FT5X06_REG_POINT_RATE;
-	ft5x06_i2c_read(client, &reg_addr, 1, &reg_value, 1);
-	if (err)
+	tries = FT_NUM_RETRY;
+	do {
+		err = ft5x06_i2c_read(client, &reg_addr, 1, &reg_value, 1);
+		msleep(FT_DELAY_DFLT);
+	} while ((err < 0) && tries--);
+
+	if (err < 0) {
 		dev_err(&client->dev, "report rate read failed");
+		goto free_reset_gpio;
+	}
 	dev_info(&client->dev, "[FTS] report rate is %dHz.\n", reg_value * 10);
 
+	/* read touch threshold */
 	reg_addr = FT5X06_REG_THGROUP;
-	err = ft5x06_i2c_read(client, &reg_addr, 1, &reg_value, 1);
-	if (err)
+	tries = FT_NUM_RETRY;
+	do {
+		err = ft5x06_i2c_read(client, &reg_addr, 1, &reg_value, 1);
+		msleep(FT_DELAY_DFLT);
+	} while ((err < 0) && tries--);
+
+	if (err < 0) {
 		dev_err(&client->dev, "threshold read failed");
+		goto free_reset_gpio;
+	}
 	dev_dbg(&client->dev, "[FTS] touch threshold is %d.\n", reg_value * 4);
 
+	/* Requesting irq */
 	err = request_threaded_irq(client->irq, NULL,
 				   ft5x06_ts_interrupt, pdata->irqflags,
 				   client->dev.driver->name, data);
