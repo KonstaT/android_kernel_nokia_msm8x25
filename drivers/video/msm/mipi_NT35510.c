c3313d0cd164e4182c88cece5be0f1bb65751bbc
Padmanabhan Komanduru <pkomandu@codeaurora.org>
2013-02-04 14:10:02 +0530

    msm_fb: Update the panel init commands for NT35510 panel
    For NT35510 client in video mode, the DCS panel init commands
    need to be updated to avoid tearing issues during camera preview
    and video playback use cases.
    
    CRs-fixed: 447081
    Change-Id: Ia8aec2b17d9e15f46b0018c301ebb89bfd5f5354
    Signed-off-by: Padmanabhan Komanduru <pkomandu@codeaurora.org>

--- drivers/video/msm/mipi_NT35510.c
+++ drivers/video/msm/mipi_NT35510.c
@@ -428,7 +428,7 @@ static char video26[6] = {
 static char video27[2] = {
 	0x35, 0x00,
 };
-static char config_video_MADCTL[2] = {0x36, 0xC0};
+
 static struct dsi_cmd_desc nt35510_video_display_on_cmds[] = {
 	{DTYPE_GEN_LWRITE, 1, 0, 0, 0, sizeof(video0), video0},
 	{DTYPE_GEN_LWRITE, 1, 0, 0, 0, sizeof(video1), video1},
@@ -464,10 +464,6 @@ static struct dsi_cmd_desc nt35510_video_display_on_cmds[] = {
 			display_on},
 };
 
-static struct dsi_cmd_desc nt35510_video_display_on_cmds_rotate[] = {
-	{DTYPE_DCS_WRITE1, 1, 0, 0, 0,
-		sizeof(config_video_MADCTL), config_video_MADCTL},
-};
 static int mipi_nt35510_lcd_on(struct platform_device *pdev)
 {
 	struct msm_fb_data_type *mfd;
@@ -491,15 +487,13 @@ static int mipi_nt35510_lcd_on(struct platform_device *pdev)
 		rotate = mipi_nt35510_pdata->rotate_panel();
 
 	if (mipi->mode == DSI_VIDEO_MODE) {
+		if (rotate)
+			video19[2] = 0x06;
+
 		mipi_dsi_cmds_tx(&nt35510_tx_buf,
 			nt35510_video_display_on_cmds,
 			ARRAY_SIZE(nt35510_video_display_on_cmds));
 
-		if (rotate) {
-			mipi_dsi_cmds_tx(&nt35510_tx_buf,
-				nt35510_video_display_on_cmds_rotate,
-			ARRAY_SIZE(nt35510_video_display_on_cmds_rotate));
-		}
 	} else if (mipi->mode == DSI_CMD_MODE) {
 		mipi_dsi_cmds_tx(&nt35510_tx_buf,
 			nt35510_cmd_display_on_cmds,
